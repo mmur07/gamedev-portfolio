2c85c33852795457e10c322639055958bbfdbdfa
mmur07 <mmurcort@gmail.com>
2022-07-16 19:55:22 +0200

    porfavor 3.0

--- deploy.sh
+++ deploy.sh
@@ -16,7 +16,10 @@ git init
 git add -A
 git commit -m 'deploy'
 
+# if you are deploying to https://<USERNAME>.github.io
+git push -f git@github.com:mmur07/mmur07.github.io.git main
+
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:mmur07/gamedev-portfolio.git main:gh-pages
+# git push -f git@github.com:mmur07/gamedev-portfolio.git main:gh-pages
 
 cd -
